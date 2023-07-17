663be2154f0235aa8e703b156b87bce21a0b3752
DavGreenberg <1kindlekid@gmail.com>
2023-07-17 00:01:40 -0400

    yaurs

--- excelHandler.js
+++ excelHandler.js
@@ -1,25 +1,17 @@
 const XLSX = require('xlsx')
-const ipc = require('electron').ipcRenderer
 
-// // initializes event listener
-// function init() {
-//     console.log('initialized listener')
-//     document.getElementById('excelFile').addEventListener('change', loadexcel, false);
-// }
+// const submitButton = document.getElementById('excelsubmit')
+// submitButton.addEventListener('click', () => {
+//     loadexcel()
+// })
 
-const submitButton = document.getElementById('excelsubmit')
-submitButton.addEventListener('click', () => {
-    document.getElementById('sheetname').innerHTML = "hello"
-})
-
-// // loads and parses excel file using xlsx
-// function loadexcel() {
-//     console.log('began load');
-//     const f = document.getElementById('excelFile');
-//     var workbook = XLSX.read(f);
-//     var sheet = workbook.getActiveSheet();
-//     document.getElementById('sheetName').innerText = sheet.getName();
-// }
+// loads and parses excel file using xlsx
+function loadexcel(form) {
+    const f = form.excelfile.files
+    var workbook = XLSX.read(f);
+    var sheet = workbook.SheetNames[0];
+    document.getElementById('sheetName').innerText = sheet;
+}
 
 // function functionthingy() {
 //     alert("alert")
